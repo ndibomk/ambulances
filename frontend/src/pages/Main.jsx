2b2b05fbbabaf1fd80b7b47944fd0d50c84c8e3a
ndibomk <83207831+petergachau@users.noreply.github.com>
2022-08-14 14:12:06 +0300

    updated

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import styled from "styled-components";
-
+// git remote set-url origin https://github.com/ndibomk/ambulances.git
 import Admin from './Admin'
 import { Link, NavLink, Outlet, useLocation } from "react-router-dom";
 
